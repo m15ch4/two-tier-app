9d8da843e713bf9c11f2b60ee971de6ac947c519
Administrator <admin@example.com>
2022-11-04 16:05:15 +0100

    initial commit

---
+++ cmd/frontend/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+type SqlPlayerStore struct {
+	db *sql.DB
+}
+
+func (s *SqlPlayerStore) GetPlayerScore(name string) int {
+	query := fmt.Sprintf("SELECT score FROM players WHERE name='%s';", name)
+	score := 0
+	err := s.db.QueryRow(query).Scan(&score)
+	if err != nil {
+		return -1
+	}
+
+	return score
+}
+
+func NewSqlPlayerStore(host string, port int32, user string, password string, dbname string) PlayerStore {
+	store := &SqlPlayerStore{}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	store.db = db
+
+	return store
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) showScore(c *gin.Context) {
+	name := c.Param("name")
+	score := p.store.GetPlayerScore(name)
+
+	c.String(http.StatusOK, "%v", score)
+}
+
+func main() {
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+	server := &PlayerServer{&store}
+
+	router := gin.Default()
+
+	router.GET("/players/:name", server.showScore)
+	router.Run(":8082")
+}
